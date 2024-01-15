e72042df611b147b4aeded8c412a7499052915be
Paulo Neto <paulo.neto@pnyta.com>
2024-01-15 14:23:46 -0300

    add env file

--- sanity/sanity.client.ts
+++ sanity/sanity.client.ts
@@ -1,7 +1,7 @@
 import { createClient, type ClientConfig } from '@sanity/client'
 
 const config: ClientConfig = {
-    projectId: 'vtmhihtu',
+    projectId: process.env.SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2023-07-16',
     useCdn: false
